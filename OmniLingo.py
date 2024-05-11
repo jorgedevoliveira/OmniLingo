c4e82cfdb3907083d48d61fbcdadc2c1b97fac4a
Jorge de Oliveira <jorge.oa@hotmail.com>
2024-05-11 04:20:29 -0300

    Commit inicial

---
+++ OmniLingo.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+
+
+# Configurando a API Key do GEMINI AI
+genai.configure(api_key="AIzaSyCk282dAS15LSTOU7GOjsmWOkoFhmMoUlI")
+
+
+# Set up the model
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+]
+
+system_instruction = "Responda como um professor de idiomas, \nna primeira resposta, solicite o idioma que a pessoa quer praticar e que para encerrar do chat basta digitar sair,\nresponda no idioma pt-br e no idioma solicitado no promp\n\n"
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                              generation_config=generation_config,
+                              system_instruction=system_instruction,
+                              safety_settings=safety_settings)
+
+# Função para interação com o chatbot
+
+def main():
+    st.title("Chatbot com GEMINI AI")
+    st.markdown("Este é um chatbot alimentado por GEMINI AI, onde você pode praticar idiomas.")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    if "chat" not in st.session_state:
+        st.session_state.chat = gemini.start_chat()  
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input(""):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        response = st.session_state.chat.send_message(prompt)
+        st.session_state.messages.append({"role": "assistant", "content": response.text})
+
+        with st.chat_message("assistant"):
+            st.markdown(response.text)
+
+if __name__ == "__main__":
+    main()
