3fa6350ceb1ad4a57e5463733610d249c0cc9577
Jorge de Oliveira <jorge.oa@hotmail.com>
2024-05-11 04:30:00 -0300

    Update OmniLingo.py

--- OmniLingo.py
+++ OmniLingo.py
@@ -52,7 +52,7 @@ def main():
         st.session_state.messages = []
 
     if "chat" not in st.session_state:
-        st.session_state.chat = gemini.start_chat()  
+        st.session_state.chat = genai.start_chat()  
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
